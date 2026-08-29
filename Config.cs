37ee983cdc3f4b9b25f2ab6beaa37ed49d1052c1
SKN <svein@sknthelisper.no>
2026-08-29 17:44:00 +0200

    Quick changes to config file.
    My original plan and idea around syncing doesn't work after testing,
    so trying to think up a better solution to it (for my own useage).

--- Config.cs
+++ Config.cs
@@ -6,11 +6,10 @@ using Playnite.SDK;
 namespace InstallFromMegaPlugin{
     public static class Config{
         private static string _configPath = "config.ini";
-        public const string LASTSYNC = "lastSync";
-        public const string MEGATOOLS = "megaToolspath";
-        public const string MEGAGAMESURL = "megagames.dbpath";
-        public const string MEGALASTUPDATEURL = "lastupdatedurl";
-        public const string DOWNLOADPATH = "downloadPath";
+        public const string LASTSYNC = "lastSync"; // When the extension was last synced for the mega.nz library. If out of sync, a reminder will be thrown.
+        public const string MEGATOOLS = "megaToolspath"; // Where the local MegaTools is setup
+        public const string MEGALASTUPDATEURL = "lastupdatedurl"; // Link to where the last-sync file is. Need to contain a dateTime timestamp
+        public const string LIBRARYPATH = "libraryurl"; // Link to where the playnite/library is on mega.nz
 
         ///<summary>Initialize the static config path to be from pluginDataPath</summary>
         ///<param name="pluginDataPath">The Playnite Plugin Data Path</param>
@@ -22,7 +21,7 @@ namespace InstallFromMegaPlugin{
         public static void CreateBlankConfigFile(IPlayniteAPI api){
             if(!File.Exists(_configPath)){
                 var configContent = new System.Text.StringBuilder();
-                string[] keys = { MEGATOOLS, LASTSYNC, MEGAGAMESURL, MEGALASTUPDATEURL, DOWNLOADPATH};
+                string[] keys = { MEGATOOLS, LASTSYNC, MEGALASTUPDATEURL, MEGALASTUPDATEURL, LIBRARYPATH};
 
                 foreach(var key in keys){
                     string value = "";
